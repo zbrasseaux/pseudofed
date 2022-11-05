cd3db5ac9874e4e8b506447b1d55e917fd822d38
Zach Brasseaux <zbrasseaux97@gmail.com>
2022-11-05 15:36:26 -0500

    test

--- main.js
+++ main.js
@@ -5,11 +5,8 @@ var fetch_args = {method:"GET", referrerPolicy:"unsafe-url"}
 var url = 'https://hci.social/api/v1/timelines/public?local=true&only_media=false';
 
 
-async function f(a,b) {
-	var promise = fetch(a,b).resolve(1);
-	return await promise;
-}
+var page_data = fetch(url, fetch_args).then({ (data) => {
+	console.log(data)
+}});
 
-var data = f(url, fetch_args);
-
-console.log(data);
+console.log(page_data);

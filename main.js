4b043b284dd9dd1279316d84bce48a39bd8667ef
Zach Brasseaux <zbrasseaux97@gmail.com>
2022-11-05 20:51:59 -0500

    link user urls to usernames in posts

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 const url = `https://hci.social/api/v1/timelines/public?local=true&only_media=false`;
 
 // list of instances, in the future, this will be stored in browser cookies
-let fed_instances = ['hci.social'];
+let fed_instances = ['hci.social', 'mastodon.social', 'indiepocalypse.social'];
 // args for fetch function, should be updated
 const fetch_args = {method:"GET", referrerPolicy:"unsafe-url"};
 
@@ -26,11 +26,28 @@ fed_instances.forEach( (value) => {
 	});
 });
 
+let count = 0;
+
 setTimeout(() => {
 	data_store.flat().forEach((value) => {
 		const content = value['content'];
 		const username = value['account']['username'];
-		data_field.innerHTML += `<h3>${username}</h3><p>${content}</p>`;
+		let dispname = value['account']['display_name'];
+		const userurl = value['account']['url'];
+		if (dispname === "") {
+			dispname = username;
+		}
+		data_field.innerHTML += 
+		`<div class="post" id="x${count%2}">
+				<a href="${userurl}"> <div class="userinfo">
+					<span class="dispname">${dispname}</span> 
+					<span class="username">@${username}</span>
+				</div> </a>
+				<div class="post-content">
+					${content}
+				</div>
+			</div>`;
+		count += 1;
 	});	
 }, 3000);
 

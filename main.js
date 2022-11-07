37ea129ba910e997ccdc0142a27c2af84a126a68
Zach Brasseaux <zbrasseaux97@gmail.com>
2022-11-06 21:58:34 -0600

    converted toots to embedded iframes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
 // temp url for testing purposes
-const url = `https://hci.social/api/v1/timelines/public?local=true&only_media=false`;
+// const url = `https://hci.social/api/v1/timelines/public?local=true&only_media=false`;
 
 // list of instances, in the future, this will be stored in browser cookies
 let fed_instances = ['hci.social', 'mastodon.social', 'indiepocalypse.social'];
@@ -9,6 +9,10 @@ let fed_instances = ['hci.social', 'mastodon.social', 'indiepocalypse.social'];
 const fetch_args = {method:"GET", referrerPolicy:"unsafe-url"};
 
 const data_field = document.getElementById('data');
+
+fed_instances.forEach( (site) => {
+	data_field.innerHTML += `<script src="https://${site}/embed.js" async="async"></script>`;
+});
 
 // convert base urls to full api urls, then to fetch promises
 fed_instances = fed_instances.map((value) => {
@@ -30,26 +34,16 @@ let count = 0;
 
 setTimeout(() => {
 	data_store.flat().forEach((value) => {
-		const content = value['content'];
-		const username = value['account']['username'];
-		let dispname = value['account']['display_name'];
-		const userurl = value['account']['url'];
-		if (dispname === "") {
-			dispname = username;
-		}
+		const url = value['uri'];
+		
+		console.log(count);
 		data_field.innerHTML += 
-		`<div class="post" id="x${count%2}">
-				<a href="${userurl}"> <div class="userinfo">
-					<span class="dispname">${dispname}</span> 
-					<span class="username">@${username}</span>
-				</div> </a>
-				<div class="post-content">
-					${content}
-				</div>
-			</div>`;
+		`<iframe onload="resizeIframe(this)" src="${url}/embed" class="mastodon-embed" style="max-width: 100%; border: 0; padding:20px;" width=700 allowfullscreen="false"></iframe>`;
 		count += 1;
 	});	
 }, 3000);
+
+
 
 
 
